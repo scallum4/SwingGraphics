ff0973e5917665fd79bc783086327043259c0410
scallum4 <48036416+scallum4@users.noreply.github.com>
2020-07-28 18:22:22 +0100

    Objects can now be added to the screen

--- Scene.java
+++ Scene.java
@@ -3,7 +3,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.Rectangle;
-import java.awt.geom.Ellipse2D.Double;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.BasicStroke;
 import java.util.ArrayList;
 
 public class Scene extends JPanel implements Runnable{
@@ -27,14 +29,42 @@ public class Scene extends JPanel implements Runnable{
     }
 
     public void paintComponent(Graphics g) {
-       /* super.paintComponent(g);
-        Graphics2D graphics = (Graphics2D)g;
-        graphics.setColor(Color.BLUE);
-        Rectangle r = new Rectangle(100, 0, tempS.getWidth(), tempS.getHeight());
+        synchronized (this){
+            Graphics2D graphics = (Graphics2D)g;
+            graphics.clearRect(0, 0, this.getWidth(), this.getHeight());
+            for(int i = 0; i < items.size(); i++){
+                DrawableObject ob = items.get(i);
 
-        graphics.rotate(Math.toRadians(45), 50 + 100, 50 + 0);
-        graphics.draw(r);
-        graphics.fill(r);*/
+                if(ob instanceof Square){
+                    Square s = (Square) ob;
+                    graphics.setColor(s.getColour());
+                    Rectangle r = new Rectangle(s.getxPos(), s.getyPos(), s.getWidth(), s.getHeight());
+                    if(s.getAngle() != 0) {
+                        graphics.rotate(Math.toRadians(s.getAngle()), (s.getWidth() / 2) + s.getxPos(), (s.getHeight() / 2) + s.getyPos());
+                        graphics.fill(r);
+                        graphics.rotate(Math.toRadians(-s.getAngle()), (s.getWidth() / 2) + s.getxPos(), (s.getHeight() / 2) + s.getyPos());
+                    } else{
+                        graphics.fill(r);
+                    }
+                } else if(ob instanceof Circle){
+                    Circle c = (Circle) ob;
+                    graphics.setColor(c.getColour());
+                    Ellipse2D.Double r = new Ellipse2D.Double(c.getxPos(), c.getyPos(), c.getWidth(), c.getHeight());
+                    if(c.getAngle() != 0) {
+                        graphics.rotate(Math.toRadians(c.getAngle()), (c.getWidth() / 2) + c.getxPos(), (c.getHeight() / 2) + c.getyPos());
+                        graphics.fill(r);
+                        graphics.rotate(Math.toRadians(-c.getAngle()), (c.getWidth() / 2) + c.getxPos(), (c.getHeight() / 2) + c.getyPos());
+                    } else{
+                        graphics.fill(r);
+                    }
+                } else if(ob instanceof Line){
+                    Line l = (Line) ob;
+                    graphics.setColor(l.getColour());
+                    graphics.setStroke(new BasicStroke((float)l.getWidth()));
+                    graphics.draw(new Line2D.Double(l.getStartX(),l.getStartY(),l.getEndX(),l.getEndY()));
+                }
+            }
+        }
     }
 
     public void addElement(DrawableObject object){
@@ -48,5 +78,9 @@ public class Scene extends JPanel implements Runnable{
             }
         }
         items.add(object);
+    }
+
+    public void removeElement(DrawableObject object){
+        items.remove(object);
     }
 }
