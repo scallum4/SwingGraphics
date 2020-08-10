3c5bb9a6db3554242a247b214708fd9da0347a87
scallum4 <48036416+scallum4@users.noreply.github.com>
2020-08-10 23:01:30 +0100

    Javadoc added.

--- DrawableObject.java
+++ DrawableObject.java
@@ -1,33 +1,52 @@
 package SwingGraphics;
-
 import java.awt.Color;
 import java.util.HashMap;
-
+/**
+ * Used to model a object that will be drawn to the screen.
+ * You should not need to create an instance of this class.
+ */
 public class DrawableObject{
-    private static final String DEFAULT_COLOUR = "BLUE";
-    private int layer;
-    private String colour;
-    private HashMap<String, Color> colours = new HashMap<>();
 
+    private static final String DEFAULT_COLOUR = "BLUE";       // The colour an object will be if the user does not specifty a colour. 
+    private int layer;                                         // The layer that the object will be drawn at (used to determine what objects will be drawn above what other objects if they are overlapping).
+    private String colour;                                     // The colour of the object. 
+    private HashMap<String, Color> colours = new HashMap<>();  // A hasmap of all the colours and their names as a string. 
+
+    /**
+     * Constructor. Creates a DrawableObject.
+     */
     public DrawableObject(){
         layer = 0;
         colour = "";
         init();
     }
 
+    /**
+     * Constructor. Creates a DrawableObject with the given parameters.
+     * @param colour The colour of the object. 
+     */
     public DrawableObject(String colour){
         layer = 0;
         this.colour = colour.toUpperCase();
         init();
     }
 
-    public DrawableObject(int layer, String colour){
-        this.layer = layer;
+    /**
+     * Constructor. Creates a DrawableObject with the given parameters.
+     * @param colour The colour of the object. 
+     * @param layer The layer that the object will be drawn at (used to determine what objects will be drawn above what other objects if they are overlapping).
+     */
+    public DrawableObject(String colour, int layer){
         this.colour = colour.toUpperCase();
+        this.layer = layer;
         init();
     }
 
-    public void init(){
+    /**
+     * Called when the class is initiated. Puts the colours in to the hashmap.
+     * Should not be called by the user. 
+     */
+    private void init(){
         colours.put("BLACK", Color.BLACK);
         colours.put("BLUE", Color.BLUE);
         colours.put("CYAN", Color.CYAN);
@@ -43,20 +62,36 @@ public class DrawableObject{
         colours.put("YELLOW", Color.YELLOW);
     }
 
+    /**
+     * Gets the layer of the object. 
+     * @return The layer of the object.
+     */
     public int getLayer() {
         return layer;
     }
 
+    /**
+     * Gets the colour of the object. 
+     * @return The colour of the object.
+     */
     public Color getColour(){
         if(colours.get(colour) == null)
             colour = DEFAULT_COLOUR;
         return colours.get(colour);
     }
 
+    /**
+     * Sets the layer of the object. 
+     * @param layer The new layer of the object.
+     */
     public void setLayer(int layer) {
         this.layer = layer;
     }
 
+    /**
+     * Sets the colour of the object. 
+     * @param colour The new layer of the object.
+     */
     public void setColour(String colour){
         this.colour = colour.toUpperCase();
     }
