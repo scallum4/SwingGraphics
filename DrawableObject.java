37b91b0a02ae7cca37b7f5ca9ea0112afb1f62db
scallum4 <48036416+scallum4@users.noreply.github.com>
2020-08-12 21:05:43 +0100

    Color now handled by Scene.

--- DrawableObject.java
+++ DrawableObject.java
@@ -7,10 +7,8 @@ import java.util.HashMap;
  */
 public class DrawableObject{
 
-    private static final String DEFAULT_COLOUR = "BLUE";       // The colour an object will be if the user does not specifty a colour. 
-    private int layer;                                         // The layer that the object will be drawn at (used to determine what objects will be drawn above what other objects if they are overlapping).
-    private String colour;                                     // The colour of the object. 
-    private HashMap<String, Color> colours = new HashMap<>();  // A hasmap of all the colours and their names as a string. 
+    private int layer;      // The layer that the object will be drawn at (used to determine what objects will be drawn above what other objects if they are overlapping).
+    private String colour;  // The colour of the object. 
 
     /**
      * Constructor. Creates a DrawableObject.
@@ -18,7 +16,6 @@ public class DrawableObject{
     public DrawableObject(){
         layer = 0;
         colour = "";
-        init();
     }
 
     /**
@@ -28,7 +25,6 @@ public class DrawableObject{
     public DrawableObject(String colour){
         layer = 0;
         this.colour = colour.toUpperCase();
-        init();
     }
 
     /**
@@ -39,27 +35,6 @@ public class DrawableObject{
     public DrawableObject(String colour, int layer){
         this.colour = colour.toUpperCase();
         this.layer = layer;
-        init();
-    }
-
-    /**
-     * Called when the class is initiated. Puts the colours in to the hashmap.
-     * Should not be called by the user. 
-     */
-    private void init(){
-        colours.put("BLACK", Color.BLACK);
-        colours.put("BLUE", Color.BLUE);
-        colours.put("CYAN", Color.CYAN);
-        colours.put("DARKGREY", Color.DARK_GRAY);
-        colours.put("GREY", Color.GRAY);
-        colours.put("GREEN", Color.GREEN);
-        colours.put("LIGHTGREY", Color.LIGHT_GRAY);
-        colours.put("MAGENTA", Color.MAGENTA);
-        colours.put("ORANGE", Color.ORANGE);
-        colours.put("PINK", Color.PINK);
-        colours.put("RED", Color.RED);
-        colours.put("WHITE", Color.WHITE);
-        colours.put("YELLOW", Color.YELLOW);
     }
 
     /**
@@ -74,10 +49,8 @@ public class DrawableObject{
      * Gets the colour of the object. 
      * @return The colour of the object.
      */
-    public Color getColour(){
-        if(colours.get(colour) == null)
-            colour = DEFAULT_COLOUR;
-        return colours.get(colour);
+    public String getColour(){
+        return colour;
     }
 
     /**
--- Scene.java
+++ Scene.java
@@ -8,18 +8,56 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.BasicStroke;
 import java.util.ArrayList;
+import java.util.HashMap;
 /**
  * Creates a JPanel that the graphics can be displayed on. 
  */
 public class Scene extends JPanel implements Runnable{
-    private ArrayList<DrawableObject> items = new ArrayList<DrawableObject>();
+
+    private static final String DEFAULT_OBJECT_COLOUR = "RED";  // Default colour for the objects that are drawn to the screen. Used if the colour the user entered isnt valid. 
+    private static final String DEFAULT_BACKGROUND_COLOUR = "WHITE"; // Default colour for the background. Used if the colour the user entered isnt valid. 
+    private String backgroundColour = "WHITE";  // The colour of the scenes background (Available colours are: BLACK, BLUE, CYAN, DARKGREY, GREY, GREEN, LIGHTGREY, MAGENTA, ORANGE, PINK, RED, WHITE, YELLOW).
+    private ArrayList<DrawableObject> items = new ArrayList<DrawableObject>();  // ArrayList that stores all the objects that will be drawn to the screen.
+    private HashMap<String, Color> colours = new HashMap<>();  // A hasmap of all the colours and their names as a string. 
 
     /**
      * Constructor. Creates a new Scene.
      */
     public Scene(){
+        init();
         Thread t = new Thread(this);
         t.start();
+    }
+
+     /**
+      * Constructor. Creates a new Scene with a specifird background colour.
+      * @param backgroundColour The colour of the background (Available colours are: BLACK, BLUE, CYAN, DARKGREY, GREY, GREEN, LIGHTGREY, MAGENTA, ORANGE, PINK, RED, WHITE, YELLOW).
+      */
+    public Scene(String backgroundColour){
+        this.backgroundColour = backgroundColour;
+        init();
+        Thread t = new Thread(this);
+        t.start();
+    }
+
+    /**
+     * Called when the class is initiated. Puts the colours in to the hashmap.
+     * Should not be called by the user. 
+     */
+    private void init(){
+        colours.put("BLACK", Color.BLACK);
+        colours.put("BLUE", Color.BLUE);
+        colours.put("CYAN", Color.CYAN);
+        colours.put("DARKGREY", Color.DARK_GRAY);
+        colours.put("GREY", Color.GRAY);
+        colours.put("GREEN", Color.GREEN);
+        colours.put("LIGHTGREY", Color.LIGHT_GRAY);
+        colours.put("MAGENTA", Color.MAGENTA);
+        colours.put("ORANGE", Color.ORANGE);
+        colours.put("PINK", Color.PINK);
+        colours.put("RED", Color.RED);
+        colours.put("WHITE", Color.WHITE);
+        colours.put("YELLOW", Color.YELLOW);
     }
 
     /**
@@ -42,13 +80,14 @@ public class Scene extends JPanel implements Runnable{
     public void paintComponent(Graphics g){
         synchronized (this){
             Graphics2D graphics = (Graphics2D)g;
+            graphics.setBackground(backgroundAsColor());
             graphics.clearRect(0, 0, this.getWidth(), this.getHeight());
             for(int i = 0; i < items.size(); i++){
                 DrawableObject ob = items.get(i);
 
                 if(ob instanceof Square){
                     Square s = (Square) ob;
-                    graphics.setColor(s.getColour());
+                    graphics.setColor(stringToColor(s.getColour()));
                     Rectangle2D.Double r = new Rectangle2D.Double(s.getxPos(), s.getyPos(), s.getWidth(), s.getHeight());
                     if(s.getAngle() != 0) {
                         graphics.rotate(Math.toRadians(s.getAngle()), (s.getWidth() / 2) + s.getxPos(), (s.getHeight() / 2) + s.getyPos());
@@ -59,7 +98,7 @@ public class Scene extends JPanel implements Runnable{
                     }
                 } else if(ob instanceof Circle){
                     Circle c = (Circle) ob;
-                    graphics.setColor(c.getColour());
+                    graphics.setColor(stringToColor(c.getColour()));
                     Ellipse2D.Double r = new Ellipse2D.Double(c.getxPos(), c.getyPos(), c.getWidth(), c.getHeight());
                     if(c.getAngle() != 0) {
                         graphics.rotate(Math.toRadians(c.getAngle()), (c.getWidth() / 2) + c.getxPos(), (c.getHeight() / 2) + c.getyPos());
@@ -70,7 +109,7 @@ public class Scene extends JPanel implements Runnable{
                     }
                 } else if(ob instanceof Line){
                     Line l = (Line) ob;
-                    graphics.setColor(l.getColour());
+                    graphics.setColor(stringToColor(l.getColour()));
                     graphics.setStroke(new BasicStroke((float)l.getWidth()));
                     graphics.draw(new Line2D.Double(l.getStartX(),l.getStartY(),l.getEndX(),l.getEndY()));
                 }
@@ -79,7 +118,28 @@ public class Scene extends JPanel implements Runnable{
     }
 
     /**
-     * Used too add an item that will be drawn to the screen. 
+     * Gets the colour of the object. 
+     * @param colour A colour of an object as a String (Available colours are: BLACK, BLUE, CYAN, DARKGREY, GREY, GREEN, LIGHTGREY, MAGENTA, ORANGE, PINK, RED, WHITE, YELLOW).
+     * @return The colour of the object.
+     */
+    public Color stringToColor(String colour){
+        if(colours.get(colour) == null)
+            return colours.get(DEFAULT_OBJECT_COLOUR);
+        return colours.get(colour);
+    }
+
+    /**
+     * Gets the colour of the background. 
+     * @return The colour of the background.
+     */
+    public Color backgroundAsColor(){
+        if(colours.get(backgroundColour) == null)
+            return colours.get(DEFAULT_BACKGROUND_COLOUR);
+        return colours.get(backgroundColour);
+    }
+
+    /**
+     * Used to add an item that will be drawn to the screen. 
      * @param object The object that will be added. 
      */
     public void addElement(DrawableObject object){
@@ -101,5 +161,21 @@ public class Scene extends JPanel implements Runnable{
      */
     public void removeElement(DrawableObject object){
         items.remove(object);
+    }
+
+    /**
+     * Gets the background colour of the scene. 
+     * @return The colour of the background as a String.
+     */
+    public String getBackgroundColour(){
+        return backgroundColour;
+    }
+
+    /**
+     * Changes the background of the scene.
+     * @param backgroundColour The colour the background will be changed to (Available colours are: BLACK, BLUE, CYAN, DARKGREY, GREY, GREEN, LIGHTGREY, MAGENTA, ORANGE, PINK, RED, WHITE, YELLOW).
+     */
+    public void setBackgroundColour(String backgroundColour){
+        this.backgroundColour = backgroundColour;
     }
 }
